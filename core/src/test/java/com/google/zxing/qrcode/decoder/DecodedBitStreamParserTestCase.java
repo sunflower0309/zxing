5d55c0eea2981397c1f17cab13d53ddf3e24c7b1
sunflower0309 <805786665@qq.com>
2020-02-27 16:26:34 -0800

    2020.2.27

--- core/src/test/java/com/google/zxing/qrcode/decoder/DecodedBitStreamParserTestCase.java
+++ core/src/test/java/com/google/zxing/qrcode/decoder/DecodedBitStreamParserTestCase.java
@@ -66,20 +66,6 @@ public class DecodedBitStreamParserTestCase extends Assert {
     assertEquals("\u00f1\u00f2\u00f3", result);
   }
 
-  @Test
-  public void testSimpleByteMode1() throws Exception {
-    BitSourceBuilder builder = new BitSourceBuilder();
-    builder.write(0x04, 4); // Byte mode
-    builder.write(0x03, 8); // 3 bytes
-    builder.write(0xF1, 8);
-    builder.write(0xF2, 8);
-    builder.write(0xF3, 8);
-
-    String result = DecodedBitStreamParser.decode(builder.toByteArray(),
-      Version.getVersionForNumber(1), null, null).getText();
-    assertEquals("\u00f1\u00f2\u00f3", result);
-  }
-
 
   @Test
   public void testSimpleSJIS() throws Exception {
